6632003c09cb0078c373312b0b0502e89b5837f5
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-08-30 13:12:40 -0400

    Changed version numbers // been a while since I've done this

--- src/main/java/com/thexfactor117/levels/Reference.java
+++ src/main/java/com/thexfactor117/levels/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 {
 	public static final String MODID = "levels";
 	public static final String NAME = "Levels";
-	public static final String VERSION = "b1.0.0";
+	public static final String VERSION = "b1.0.1";
 	public static final String COMMON_PROXY = "com.thexfactor117.levels.proxies.CommonProxy";
 	public static final String CLIENT_PROXY = "com.thexfactor117.levels.proxies.ClientProxy";
 }
