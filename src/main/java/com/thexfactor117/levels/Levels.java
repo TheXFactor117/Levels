14f1b21d65120d9902d05cd929aa840cf10476e7
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-09-07 20:36:30 -0400

    Organizing main class

--- src/main/java/com/thexfactor117/levels/Levels.java
+++ src/main/java/com/thexfactor117/levels/Levels.java
@@ -5,18 +5,17 @@ import com.thexfactor117.levels.helpers.LogHelper;
 import com.thexfactor117.levels.init.ModEvents;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 /**
  * To-Do List
  * b3.0.0
+ * - Mod Compatibility!
+ * 
+ * b4.0.0
  * - Bow Leveling System
  * - Bow Abilities
  * - Bow Crafting Recipes
- * 
- * b4.0.0
- * - Mod integration
  */
 
 /**
@@ -31,16 +30,10 @@ public class Levels
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		LogHelper.info("Beginning initialization phases...");
+		
 		ConfigHandler.registerConfig(event.getSuggestedConfigurationFile());
-		//ModItems.registerItems();
 		ModEvents.registerEvents();
-		//ModIntegration.registerIntegration();
-	}
-	
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event)
-	{
-		//ModRecipes.registerRecipes();
-		LogHelper.info("Levels has finished initializing!");
+		
+		LogHelper.info("Levels has finished initializing.");
 	}
 }
