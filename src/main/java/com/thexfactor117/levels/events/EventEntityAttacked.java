fcca300f217d9866259deecfc7793af59daee8be
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-09-08 18:52:51 -0400

    Fixes issue with Slimes/Magma Cubes experience duplication glitch

--- src/main/java/com/thexfactor117/levels/events/EventEntityAttacked.java
+++ src/main/java/com/thexfactor117/levels/events/EventEntityAttacked.java
@@ -3,6 +3,7 @@ package com.thexfactor117.levels.events;
 import java.util.Random;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
@@ -131,72 +132,75 @@ public class EventEntityAttacked
 			EntityLivingBase attacker = (EntityLivingBase) event.source.getEntity();
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
 			
-			for (int i = 0; i < 4; i++)
+			if (attacker instanceof EntityMob)
 			{
-				if (player.getCurrentArmor(i) != null)
+				for (int i = 0; i < 4; i++)
 				{
-					if (player.getCurrentArmor(i).getItem() instanceof ItemArmor && player.getCurrentArmor(i).getTagCompound() != null)
+					if (player.getCurrentArmor(i) != null)
 					{
-						NBTTagCompound nbt = player.getCurrentArmor(i).getTagCompound();
-						
-						if (nbt.getInteger("LEVEL") > 3)
+						if (player.getCurrentArmor(i).getItem() instanceof ItemArmor && player.getCurrentArmor(i).getTagCompound() != null)
 						{
-							int var = rand.nextInt(3);
-							int var1 = rand.nextInt(3);
-							if (var == 0)
+							NBTTagCompound nbt = player.getCurrentArmor(i).getTagCompound();
+							
+							if (nbt.getInteger("LEVEL") > 3)
 							{
-								nbt.setInteger("EXPERIENCE", nbt.getInteger("EXPERIENCE") + 1 + var1);
-							}
-						}
-						else
-						{
-							nbt.setInteger("EXPERIENCE", nbt.getInteger("EXPERIENCE") + 1);
-						}
-						
-						if (nbt != null)
-						{
-							if (nbt.getBoolean("HARDENED"))
-							{
-								int var = rand.nextInt(5);
+								int var = rand.nextInt(3);
+								int var1 = rand.nextInt(3);
 								if (var == 0)
 								{
-									attacker.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 20*2, 10));
+									nbt.setInteger("EXPERIENCE", nbt.getInteger("EXPERIENCE") + 1 + var1);
 								}
 							}
-							
-							if (nbt.getBoolean("POISONED"))
+							else
 							{
-								int var = rand.nextInt(5);
-								if (var == 0)
-								{
-									attacker.addPotionEffect(new PotionEffect(Potion.poison.id, 20*10, 0));
-								}
+								nbt.setInteger("EXPERIENCE", nbt.getInteger("EXPERIENCE") + 1);
 							}
 							
-							if (nbt.getBoolean("Strength"))
+							if (nbt != null)
 							{
-								int var = rand.nextInt(10);
-								if (var == 0)
+								if (nbt.getBoolean("HARDENED"))
 								{
-									player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20*10, 0));
+									int var = rand.nextInt(5);
+									if (var == 0)
+									{
+										attacker.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 20*2, 10));
+									}
 								}
-							}
-							
-							if (nbt.getBoolean("ETHEREAL"))
-							{
-								int var = rand.nextInt(20);
-								if (var == 0)
+								
+								if (nbt.getBoolean("POISONED"))
 								{
-									player.setHealth(20.0F);
+									int var = rand.nextInt(5);
+									if (var == 0)
+									{
+										attacker.addPotionEffect(new PotionEffect(Potion.poison.id, 20*10, 0));
+									}
 								}
-							}
-							
-							if (nbt.getBoolean("VOID"))
-							{
-								int var = rand.nextInt(20);
-								if (var == 0)
+								
+								if (nbt.getBoolean("Strength"))
 								{
-									attacker.setHealth(0);
+									int var = rand.nextInt(10);
+									if (var == 0)
+									{
+										player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20*10, 0));
+									}
+								}
+								
+								if (nbt.getBoolean("ETHEREAL"))
+								{
+									int var = rand.nextInt(20);
+									if (var == 0)
+									{
+										player.setHealth(20.0F);
+									}
+								}
+								
+								if (nbt.getBoolean("VOID"))
+								{
+									int var = rand.nextInt(20);
+									if (var == 0)
+									{
+										attacker.setHealth(0);
+									}
 								}
 							}
 						}
