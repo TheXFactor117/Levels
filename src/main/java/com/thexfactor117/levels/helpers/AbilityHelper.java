024d8d3062eb5d53bfa32ca37f5c1590581e32f1
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-09-15 20:56:29 -0400

    Did I just write an entirely new ability selection system?

--- src/main/java/com/thexfactor117/levels/helpers/AbilityHelper.java
+++ src/main/java/com/thexfactor117/levels/helpers/AbilityHelper.java
@@ -11,7 +11,146 @@ import net.minecraft.nbt.NBTTagCompound;
  *
  */
 public class AbilityHelper 
-{
+{	
+	/**
+	 * New System
+	 */
+	public static void getRandomizedMeleeAbilities(ItemStack stack, int level)
+	{
+		NBTTagCompound nbt = stack.getTagCompound();
+		
+		if (nbt != null && level >= 2)
+		{	
+			RandomCollection<String> abilityLevelCollection = new RandomCollection<String>();
+			
+			if (level == 2)
+			{
+				abilityLevelCollection.add(0.8D, "L2A1");
+				abilityLevelCollection.add(0.15D, "L2A2");
+				abilityLevelCollection.add(0.05D, "L2A3");
+				String abilityLevel = abilityLevelCollection.next();
+				
+				if (abilityLevel == "L2A1") addA1Abilities(nbt);
+				if (abilityLevel == "L2A2") addA2Abilities(nbt);
+				if (abilityLevel == "L2A3") addA3Abilities(nbt);
+			}
+			
+			if (level == 3)
+			{
+				abilityLevelCollection.add(0.7D, "L3A1");
+				abilityLevelCollection.add(0.2D, "L3A2");
+				abilityLevelCollection.add(0.1D, "L3A3");
+				String abilityLevel = abilityLevelCollection.next();
+				
+				if (abilityLevel == "L3A1") addA1Abilities(nbt);
+				if (abilityLevel == "L3A2") addA2Abilities(nbt);
+				if (abilityLevel == "L3A3") addA3Abilities(nbt);
+			}
+			
+			if (level == 4)
+			{
+				abilityLevelCollection.add(0.6D, "L4A1");
+				abilityLevelCollection.add(0.25D, "L4A2");
+				abilityLevelCollection.add(0.15D, "L4A3");
+				String abilityLevel = abilityLevelCollection.next();
+				
+				if (abilityLevel == "L4A1") addA1Abilities(nbt);
+				if (abilityLevel == "L4A2") addA2Abilities(nbt);
+				if (abilityLevel == "L4A3") addA3Abilities(nbt);
+			}
+			
+			if (level == 5)
+			{
+				abilityLevelCollection.add(0.5D, "L5A1");
+				abilityLevelCollection.add(0.3D, "L5A2");
+				abilityLevelCollection.add(0.2D, "L5A3");
+				String abilityLevel = abilityLevelCollection.next();
+				
+				if (abilityLevel == "L5A1") addA1Abilities(nbt);
+				if (abilityLevel == "L5A2") addA2Abilities(nbt);
+				if (abilityLevel == "L5A3") addA3Abilities(nbt);
+			}
+			
+			if (level == 6)
+			{
+				abilityLevelCollection.add(0.4D, "L6A1");
+				abilityLevelCollection.add(0.35D, "L6A2");
+				abilityLevelCollection.add(0.25D, "L6A3");
+				String abilityLevel = abilityLevelCollection.next();
+				
+				if (abilityLevel == "L6A1") addA1Abilities(nbt);
+				if (abilityLevel == "L6A2") addA2Abilities(nbt);
+				if (abilityLevel == "L6A3") addA3Abilities(nbt);
+			}
+		}
+	}
+	
+	public static void addA1Abilities(NBTTagCompound nbt)
+	{
+		RandomCollection<String> abilitiesCollection = new RandomCollection<String>();
+		
+		abilitiesCollection.add(0.4D, "fire");
+		abilitiesCollection.add(0.4D, "frost");
+		abilitiesCollection.add(0.07D, "poison");
+		abilitiesCollection.add(0.07D, "strength");
+		abilitiesCollection.add(0.03D, "ethereal");
+		abilitiesCollection.add(0.03D, "void");
+		String abilities = abilitiesCollection.next();
+		
+		if (abilities == "fire") nbt.setBoolean("FIRE", true);
+		if (abilities == "frost") nbt.setBoolean("FROST", true);
+		if (abilities == "poison") nbt.setBoolean("POISON", true);
+		if (abilities == "strength") nbt.setBoolean("STRENGTH", true);
+		if (abilities == "ethereal") nbt.setBoolean("ETHEREAL", true);
+		if (abilities == "void") nbt.setBoolean("VOID", true);
+	}
+	
+	public static void addA2Abilities(NBTTagCompound nbt)
+	{
+		RandomCollection<String> abilitiesCollection = new RandomCollection<String>();
+		
+		abilitiesCollection.add(0.15D, "fire");
+		abilitiesCollection.add(0.15D, "frost");
+		abilitiesCollection.add(0.25D, "poison");
+		abilitiesCollection.add(0.25D, "strength");
+		abilitiesCollection.add(0.10D, "ethereal");
+		abilitiesCollection.add(0.10D, "void");
+		String abilities = abilitiesCollection.next();
+		
+		if (abilities == "fire") nbt.setBoolean("FIRE", true);
+		if (abilities == "frost") nbt.setBoolean("FROST", true);
+		if (abilities == "poison") nbt.setBoolean("POISON", true);
+		if (abilities == "strength") nbt.setBoolean("STRENGTH", true);
+		if (abilities == "ethereal") nbt.setBoolean("ETHEREAL", true);
+		if (abilities == "void") nbt.setBoolean("VOID", true);
+	}
+	
+	public static void addA3Abilities(NBTTagCompound nbt)
+	{
+		RandomCollection<String> abilitiesCollection = new RandomCollection<String>();
+		
+		abilitiesCollection.add(0.10D, "fire");
+		abilitiesCollection.add(0.10D, "frost");
+		abilitiesCollection.add(0.15D, "poison");
+		abilitiesCollection.add(0.15D, "strength");
+		abilitiesCollection.add(0.25D, "ethereal");
+		abilitiesCollection.add(0.25D, "void");
+		String abilities = abilitiesCollection.next();
+		
+		if (abilities == "fire") nbt.setBoolean("FIRE", true);
+		if (abilities == "frost") nbt.setBoolean("FROST", true);
+		if (abilities == "poison") nbt.setBoolean("POISON", true);
+		if (abilities == "strength") nbt.setBoolean("STRENGTH", true);
+		if (abilities == "ethereal") nbt.setBoolean("ETHEREAL", true);
+		if (abilities == "void") nbt.setBoolean("VOID", true);
+	}
+	
+	
+	
+	/**
+	 * Old System
+	 */
+	
 	/**
 	 * Draws between the given abilities, picking one to be equipped to the weapon.
 	 * Default percentages are 50-50 for either Fire or Frost.
