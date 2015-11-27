eec7e89164b1fe5d81e274416665aca9bb0583da
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-11-27 13:55:16 -0500

    r2.0.3

--- src/main/java/com/thexfactor117/levels/Reference.java
+++ src/main/java/com/thexfactor117/levels/Reference.java
@@ -12,32 +12,13 @@ public class Reference
 {
 	public static final String MODID = "levels";
 	public static final String NAME = "Levels";
-	public static final String VERSION = "r2.0.2";
+	public static final String VERSION = "r2.0.3";
 	public static final String COMMON_PROXY = "com.thexfactor117.levels.proxies.CommonProxy";
 	public static final String CLIENT_PROXY = "com.thexfactor117.levels.proxies.ClientProxy";
 	public static final String UPDATE_STRING = EnumChatFormatting.GOLD + "[Levels] " + EnumChatFormatting.GRAY + StatCollector.translateToLocal("levels.update");
 	public static final String CHANGELOG_STRING = EnumChatFormatting.GRAY + StatCollector.translateToLocal("levels.changelog");
 	
 	public static final int MAX_LEVEL = 20;
-	public static final int L2_EXP = 50;
-	public static final int L3_EXP = 125;
-	public static final int L4_EXP = 225;
-	public static final int L5_EXP = 375;
-	public static final int L6_EXP = 600;
-	public static final int L7_EXP = 925;
-	public static final int L8_EXP = 1375;
-	public static final int L9_EXP = 2000;
-	public static final int L10_EXP = 2850;
-	public static final int L11_EXP = 4000;
-	public static final int L12_EXP = 5550;
-	public static final int L13_EXP = 7625;
-	public static final int L14_EXP = 10400;
-	public static final int L15_EXP = 14100;
-	public static final int L16_EXP = 19025;
-	public static final int L17_EXP = 25575;
-	public static final int L18_EXP = 34275;
-	public static final int L19_EXP = 45825;
-	public static final int L20_EXP = 61150;
 	
 	public static final int MONSTER_BONUS_EXP = 10;
 	public static final int ANIMAL_BONUS_EXP = 2;
