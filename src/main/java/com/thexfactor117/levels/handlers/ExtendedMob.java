4191b008605d42c006a5db7f11c7a4416109cccd
TheXFactor117 <thexfactor117.mods@gmail.com>
2016-03-13 18:10:29 -0400

    Somehow this didn't make it

---
+++ src/main/java/com/thexfactor117/levels/handlers/ExtendedMob.java
@@ -0,0 +1,88 @@
+package com.thexfactor117.levels.handlers;
+
+import java.util.Random;
+
+import com.thexfactor117.levels.helpers.EnemyLevel;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+public class ExtendedMob implements IExtendedEntityProperties
+{
+	public static final String EXTENDED_PROPERTIES = "ExtendedMob";
+	@SuppressWarnings("unused")
+	private final EntityMob entity;
+	private EnemyLevel level;
+	
+	/**
+	 * Constructor for the extended mob. Used to set a random level.
+	 * @param mob
+	 */
+	public ExtendedMob(EntityMob mob)
+	{
+		this.entity = mob;
+		NBTTagCompound nbt = mob.getEntityData();
+		this.level = EnemyLevel.getEnemyLevel(nbt);
+	}
+	
+	/**
+	 * Registers the mob to have extended properties
+	 * @param mob
+	 */
+	public static final void register(EntityMob mob)
+	{
+		mob.registerExtendedProperties(EXTENDED_PROPERTIES, new ExtendedMob(mob));
+	}
+	
+	/**
+	 * Returns the extended mob with its properties
+	 * @param mob
+	 * @return
+	 */
+	public static final ExtendedMob get(EntityMob mob)
+	{
+		return (ExtendedMob) mob.getExtendedProperties(EXTENDED_PROPERTIES);
+	}
+	
+	@Override
+	public void saveNBTData(NBTTagCompound compound)
+	{
+		NBTTagCompound nbt = new NBTTagCompound();
+		this.level.setEnemyLevel(nbt);
+		compound.setTag(EXTENDED_PROPERTIES, nbt);
+	}
+	
+	@Override
+	public void loadNBTData(NBTTagCompound compound)
+	{
+		NBTTagCompound nbt = (NBTTagCompound) compound.getTag(EXTENDED_PROPERTIES);
+		this.level = EnemyLevel.getEnemyLevel(nbt);
+	}
+	
+	@Override
+	public void init(Entity entity, World world) {}
+	
+	/**
+	 * Returns the enemy level as saved in its properties
+	 * @return
+	 */
+	public EnemyLevel getEnemyLevelFromProps()
+	{
+		Random rand = new Random();
+		
+		if (level == EnemyLevel.DEFAULT)
+		{
+			level = EnemyLevel.getRandomLevel(rand);
+		}
+		
+		return level;
+	}
+}
