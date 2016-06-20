6ca5282d6870c1ad05a5e64a5fc2926be9573b8e
TheXFactor117 <thexfactor117.mods@gmail.com>
2016-06-20 11:28:36 -0400

    Working on artifacts

--- src/main/java/com/thexfactor117/levels/Reference.java
+++ src/main/java/com/thexfactor117/levels/Reference.java
@@ -9,10 +9,11 @@ public class Reference
 {
 	public static final String MODID = "levels";
 	public static final String NAME = "Levels";
-	public static final String VERSION = "r3.0.3";
+	public static final String VERSION = "@VERSION@";
 	public static final String DEPENDENCIES = "required-after:xlib";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/TheXFactor117/Levels/master/levels-version-checker.json";
 	public static final String COMMON_PROXY = "com.thexfactor117.levels.proxies.CommonProxy";
 	public static final String CLIENT_PROXY = "com.thexfactor117.levels.proxies.ClientProxy";
+	public static final String MC_VERSION = "@MVERSION@";
 	public static final int MAX_ABILITY_LEVEL = 5;
 }
